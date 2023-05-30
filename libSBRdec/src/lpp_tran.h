58ca52c5d44edb1173ab701488467df58b0eebd4
Fraunhofer IIS FDK <audio-fdk@iis.fraunhofer.de>
2023-05-30 16:39:32 +0200

    Increase patchParam array size by one and fix out-of-bounce write in resetLppTransposer().
    Bug: 279766766
    Test: see POC
    (cherry picked from commit f682b8787eb312b9f8997dac4c2c18bb779cf0df)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:451762ca48e7fb30a0ce77a8962813a3419ec420)
    Merged-In: I206973e0bb21140865efffd930e39f920f477359
    Change-Id: I206973e0bb21140865efffd930e39f920f477359

--- libSBRdec/src/lpp_tran.h
+++ libSBRdec/src/lpp_tran.h
@@ -2,8 +2,8 @@
 /* -----------------------------------------------------------------------------------------------------------
 Software License for The Fraunhofer FDK AAC Codec Library for Android
 
-© Copyright  1995 - 2013 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
-  All rights reserved.
+Â© Copyright  1995 - 2023 Fraunhofer-Gesellschaft zur FÃ¶rderung der angewandten
+Forschung e.V. All rights reserved.
 
  1.    INTRODUCTION
 The Fraunhofer FDK AAC Codec Library for Android ("FDK AAC Codec") is software that implements
@@ -175,7 +175,8 @@ typedef struct {
   UCHAR  lbStopPatching;              /*!< first band that won't be patched anymore*/
   UCHAR  bwBorders[MAX_NUM_NOISE_VALUES]; /*!< spectral bands with different inverse filtering levels */
 
-  PATCH_PARAM    patchParam[MAX_NUM_PATCHES]; /*!< new parameter set for patching */
+  PATCH_PARAM
+  patchParam[MAX_NUM_PATCHES + 1];    /*!< new parameter set for patching */
   WHITENING_FACTORS whFactors;                /*!< the pole moving factors for certain whitening levels as indicated
                                                    in the bitstream depending on the crossover frequency */
   UCHAR  overlap;                             /*!< Overlap size */
